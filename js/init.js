1af77499bc8d6369cdc8ad82e76de84bcaacbb4c
David A. Lareo <dalareo@gmail.com>
2016-01-15 22:06:59 +0100

    js syntax

--- js/init.js
+++ js/init.js
@@ -3,10 +3,7 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    $(".dropdown-button").dropdown({
-        hover = false
-      }
-    );
+    $('.dropdown-button').dropdown();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
