fcc829363059a685cf36d36530912a2e0f7e9163
dalareo <dalareo@gmail.com>
2016-02-06 13:47:22 +0100

    OSS catalog

--- js/init.js
+++ js/init.js
@@ -4,6 +4,7 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.dropdown-button').dropdown();
+    $('#Container').mixItUp();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
