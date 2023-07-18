1c78dac9849521fadb6494f4641159bc988fb30c
David A. Lareo <david@eutika.com>
2023-07-18 17:56:12 +0200

    Updates

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,1 @@
-var SITE;SITE=(()=>{"use strict";var e={576:(e,r,t)=>{t.r(r)}},r={};function t(o){if(r[o])return r[o].exports;var n=r[o]={exports:{}};return e[o](n,n.exports,t),n.exports}return t.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},t(576)})();
+var SITE;(()=>{"use strict";var e={r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},r={};e.r(r),SITE=r})();
